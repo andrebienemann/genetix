bdedd5e12f16a9ac5e4429af7a675121d0c8ced9
andrebienemann <andre.bienemann@gmail.com>
2022-04-29 22:27:44 +0200

    Refactor access_uniport

--- genetix/data/uniport.py
+++ genetix/data/uniport.py
@@ -5,10 +5,9 @@ def access_uniport(prot_id: str) -> str:
     """
     Given a protein id, queries the uniport API
     and returns the corresponding protein string
-    
+
     Parameters
     prot_id: the protein id to query for
     """
-    url = f"http://www.uniprot.org/uniprot/{prot_id}.fasta"
-    response = urlopen(url)
+    response = urlopen(f"http://www.uniprot.org/uniprot/{prot_id}.fasta")
     return "".join([line.decode("utf-8") for line in response])
